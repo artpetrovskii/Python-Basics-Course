dc313668423cb496275bc2c6fc99cb4eeaded7ef
Artemii <artemii.petrovskii@outlook.com>
2020-08-26 15:19:44 +0300

    Lesson4

---
+++ hw04-normal.py
@@ -0,0 +1,144 @@
+# Задание-1: Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+
+import os
+import pickle
+import random
+import re
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO' \
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK' \
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn' \
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa' \
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete' \
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ' \
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb' \
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC' \
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB' \
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT' \
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu' \
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB' \
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa' \
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ' \
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+# 1. Решение с помощью re
+print('Задача 1')
+line_str = re.findall(r'[a-z]+', line)
+print('Символы в нижнем регистре с использованием модуля re: \n', line_str)
+
+# 2. Решение без re
+symbol = list(map(lambda x: chr(x), list(range(65, 91))))
+line_new = list(line)
+
+for i, element in enumerate(line_new[:]):
+    for element_2 in symbol:
+        if element == element_2:
+            line_new[i] = ' '
+
+stroka = ''.join(line_new).split(' ')
+
+line_str_2 = [i for i in stroka if i != '']
+print('Символы в нижнем регистре без использованием модуля re: \n', line_str_2)
+
+
+# Задание-2: Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm' \
+         'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV' \
+         'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA' \
+         'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV' \
+         'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW' \
+         'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC' \
+         'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR' \
+         'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm' \
+         'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn' \
+         'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS' \
+         'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf' \
+         'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH' \
+         'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN' \
+         'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ' \
+         'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+# 1. Решение с помощью re
+print('-' * 50)
+print('Задача 2')
+line_2_str = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print('Список с использованием модуля re: \n', line_2_str)
+
+# 2. Решение без re
+symbol1 = list(map(lambda x: chr(x), list(range(65, 91))))
+symbol2 = list(map(lambda x: chr(x), list(range(97, 123))))
+line_new = list(line_2)
+
+lst = []
+i = len(line_new) - 1
+while i >= 0:
+    if line_new[i] in symbol2:
+        lst.append(line_new[i])
+    elif line_new[i] in symbol1 and i <= len(line_new) - 3 and line_new[i + 1] in symbol1 and line_new[
+        i + 2] in symbol1:
+        lst.append(line_new[i])
+    else:
+        lst.append(' ')
+    i -= 1
+lst.reverse()
+
+i = 0
+lst2 = []
+registr = True
+while i <= len(lst) - 1:
+    if lst[i] in symbol2:
+        registr = True
+    if lst[i] in symbol1 and lst[i - 1] in symbol2 and lst[i - 2] in symbol2:
+        lst2.append(lst[i])
+        registr = False
+    elif lst[i] in symbol1 and registr == False:
+        lst2.append(lst[i])
+    else:
+        lst2.append(' ')
+    i += 1
+stroka = ''.join(lst2).split(' ')
+
+line_str_3 = [i for i in stroka if i != '']
+print('Список без использованием модуля re: \n', line_str_3)
+
+
+# Задание-3: Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+print('-' * 50)
+print('Задача 3')
+with open(os.path.join('2500 numbers.txt'), 'wb') as f:
+    a = random.randint(10 ** 2499, (10 ** 2500) - 1)
+    a = str(a)
+    pickle.dump(a, f)
+with open(os.path.join('2500 numbers.txt'), 'rb') as f:
+    reed_data = pickle.load(f)
+    print('Произвольное 2500-значное число:', reed_data, '\nСамые длинные последовательности цифр по каждому числу:')
+    maxLine = ['']
+    i = 0
+    while i < 10:
+        pattern3 = i
+        pattern3 = str(pattern3) + '+'
+        numList = re.findall(pattern3, reed_data)
+        numList.sort()
+        print(numList[-1])
+        if len(maxLine[0]) < len(numList[-1]):
+            maxLine = [numList[-1]]
+        elif len(maxLine[0]) == len(numList[-1]):
+            maxLine.append(numList[-1])
+        i += 1
+    print('\nСамая длинная последовательность цифр в случайном 2500-значном числе, состоит из',
+          len(maxLine[0]), 'цифр\nКоличество таких последовательностей:', len(maxLine), '\nСписок последовательностей:',
+          maxLine)
